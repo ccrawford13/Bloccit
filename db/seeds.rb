f8fec40314e6670c805de17ec225e831691d41ac
Chad Crawford <ccrawforduwlax@gmail.com>
2015-03-07 22:44:05 -0600

    Complete Seed Data assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require "faker"
 
-#Create 50 Posts
+# Create 50 Posts
 
 50.times do
   Post.create!(
@@ -8,7 +8,28 @@ require "faker"
     body: Faker::Lorem.paragraph
   )
 end
+
+if Post.exists?(title: 'Unique Post')
+   puts "That post currently exists in database"
+else
+  Post.create!(
+    title: "Uniques Post",
+    body: "Unique body"
+    )
+  puts "New Post Created"
+end
+  
 posts = Post.all
+
+if Comment.exists?(body: 'Unique comment')
+  puts "That comment currently exists in database"
+else
+  Comment.create!(
+    post: posts.sample,
+    body: "Uniques comment"
+    )
+  puts "New Comment Created"
+end
 
 # Create Comments
 100.times do
@@ -18,6 +39,6 @@ posts = Post.all
   )
 end
 
-puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts puts "Seed finished"
+puts "#{Post.count} posts in the database"
+puts "#{Comment.count} Comments in the database"
