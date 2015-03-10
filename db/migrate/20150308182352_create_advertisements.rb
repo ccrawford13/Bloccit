c19299e6b5cedaca3d29fb0329056e3a0375dff3
Chad Crawford <ccrawforduwlax@gmail.com>
2015-03-10 12:30:14 -0500

    Updating changes for Heroku Error

--- db/migrate/20150308182352_create_advertisements.rb
+++ db/migrate/20150308182352_create_advertisements.rb
@@ -4,9 +4,7 @@ class CreateAdvertisements < ActiveRecord::Migration
       t.string :title
       t.text :copy
       t.integer :price
-
       t.timestamps null: false
     end
-    add_primary_key :id
   end
 end
