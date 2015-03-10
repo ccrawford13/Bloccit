6386e6a717096788ed7d20baa7116804e86ff8f3
Chad Crawford <ccrawforduwlax@gmail.com>
2015-03-09 21:29:07 -0500

    Add devise authentication system

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-
+gem 'figaro', '1.0'
 gem 'pry-rails'
 gem 'faker'
 gem 'devise'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Overrid Action Mailer's silent errors in development
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -41,5 +41,4 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: 'localhost:3000'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-
 end
