0f9059232c2c284a16c858c513649ce350322eb1
Chad Crawford <ccrawforduwlax@gmail.com>
2015-03-10 19:52:42 -0500

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,11 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   #Sets the order of posts to 'descending' by time of post
-  default_scope { order('created_at DESC') }
+  # default_scope { order('created_at DESC') }
+  
+  #ASC = A-Z Sort, DESC = Z-A Sort
+  scope :ordered_by_title, -> { order('title DESC') }
+  #Sorted to show oldest post first - Could use similar ordering for Comments?
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC')}
+
 end
