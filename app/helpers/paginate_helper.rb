e5bdb2f74a338c0c371cb507e5c00c4aefd69579
Chad Crawford <ccrawforduwlax@gmail.com>
2015-03-24 20:21:28 -0500

    Complete paginate-assignment

--- app/helpers/paginate_helper.rb
+++ app/helpers/paginate_helper.rb
@@ -6,19 +6,20 @@ module PaginateHelper
   def will_paginate(collection)
     prev_link = link_to 'Previous', "?page=#{current_page - 1}"
     next_link = link_to 'Next', "?page=#{current_page + 1}"
-    
-    if current_page > 0 && current_page < @topics[:total_pages]
-      content_tag(:ul) do
-        content_tag(:li) { prev_link } +
-        content_tag(:li) { next_link } 
-      end
-    elsif current_page < 1 && current_page < @topics[:total_pages]
-      content_tag(:ul) do
-        content_tag(:li) { next_link }
-      end
-    elsif current_page >= @topics[:total_pages]
-      content_tag(:ul) do
-        content_tag(:li) { prev_link }
+    content_tag(:div, class: 'paginate') do
+      if current_page > 0 && current_page < collection[:total_pages] 
+        content_tag(:ul) do
+          content_tag(:li) { prev_link } +
+          content_tag(:li) { next_link } 
+        end
+      elsif current_page < 1 && current_page < collection[:total_pages] 
+        content_tag(:ul) do
+          content_tag(:li) { next_link }
+        end
+      elsif current_page >= collection[:total_pages] 
+        content_tag(:ul) do
+          content_tag(:li) { prev_link }
+        end
       end
     end
   end
--- lib/paginate.rb
+++ lib/paginate.rb
@@ -1,16 +1,16 @@
 module Paginate
-  
+ 
   def paginate(params)
     per_page = params[:per_page] || 20
-    page = params[:page] || 0
+    page = params[:page].to_i || 0
     total_records = count
     total_pages = (total_records / per_page).to_i
     total_pages += 1 unless total_records % per_page
-    results = limit(per_page).offset(page * 10)
-    {
-      results: results,
-      total_pages: total_pages
-    }
+      results = limit(per_page).offset(page * 10)  
+        {     
+        results: results,
+        total_pages: total_pages
+        }
   end
 end
 
